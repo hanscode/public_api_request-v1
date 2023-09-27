5f6db9924ca832ad0310e76797235dd39c516dbb
Hans Steffens <hans@steffens.co>
2023-09-26 20:14:02 -0500

    Extra Credit: filter the directory by name

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,8 @@ Author - Hans Steffens
 ******************************************/
 
 const usersUrl = 'https://randomuser.me/api/?results=12&inc=name,email,location,picture,cell,dob,nat&nat=us';
+const search = document.querySelector('.search-container');
+const h1 = document.querySelector('h1');
 const gallery = document.querySelector('.gallery');
 const body = document.querySelector('body');
 
@@ -12,10 +14,15 @@ const body = document.querySelector('body');
 //  FETCH FUNCTIONS
 // ------------------------------------------
 
-fetch(usersUrl)
-    .then(response => response.json())
-    .then(data => getEmployees(data.results))
-    .catch(error => console.log('Looks like there was a problem!', error))
+async function getJSON(url) {
+    const response = await fetch(url);
+    const users = await response.json();
+    return users;
+}
+
+getJSON(usersUrl)
+  .then(data => getEmployees(data.results))
+  .catch(error => console.log('Looks like there was a problem!', error))
 
 // ------------------------------------------
 //  HELPER FUNCTIONS
@@ -75,6 +82,40 @@ function getBirthdayFormat(date) {
     return `${month}/${day}/${year}`;
 }
 
+function appendSearch() {
+    const form = `<form action="#" method="get">
+                    <input type="search" id="search-input" class="search-input" placeholder="Search...">
+                    <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+                </form>`;
+    search.innerHTML = form;
+}
+
+appendSearch();
+
+const filterEmployees = (e) => {
+    let currentValue = e.target.value.toLowerCase();
+    let employees = document.querySelectorAll('h3.card-name');
+    let addText = '';
+    const results = [];
+    
+    employees.forEach(employee => {
+        if (employee.textContent.toLocaleLowerCase().includes(currentValue)) {
+                employee.parentNode.parentNode.style.display = 'flex';
+                results.push(employee);
+        } else {
+            employee.parentNode.parentNode.style.display = 'none';
+        }
+     });
+     // If no matches are found for an employee search, display a “No results found” type message on the page.
+     if (results.length === 0) {
+        addText = 'no results found';
+        h1.textContent = addText.toUpperCase();
+     } else {
+        addText = 'awesome startup employee directory';
+        h1.textContent = addText.toUpperCase();
+     }
+}
+
 // ------------------------------------------
 //  EVENT LISTENERS
 // ------------------------------------------
@@ -83,3 +124,11 @@ function triggerModal(employee, index) {
     const employeeID = document.getElementById(`${index}`);
     employeeID.addEventListener('click', (e) => createModal(employee));
 }
+
+const employeeSearch = document.getElementById('search-input');
+employeeSearch.addEventListener('keyup', filterEmployees);
+
+const searchForm = document.querySelector('form');
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+});
